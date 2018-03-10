368b1d10657dc0321b22a76ea3ba4dc68d0af91c
Roman <roman.anistratenkolmc@gmail.com>
2018-03-10 13:44:33 +0300

    Add get top view controller extension

---
+++ extensions/UIApplication.swift
@@ -0,0 +1,20 @@
+// MARK: - Get top view  controller
+
+extension UIApplication {
+
+    class func topViewController(base: UIViewController? = UIApplication.sharedApplication().keyWindow?.rootViewController) -> UIViewController? {
+        if let nav = base as? UINavigationController {
+            return topViewController(base: nav.visibleViewController)
+        }
+        if let tab = base as? UITabBarController {
+            if let selected = tab.selectedViewController {
+                return topViewController(base: selected)
+            }
+        }
+        if let presented = base?.presentedViewController {
+            return topViewController(base: presented)
+        }
+        return base
+    }
+
+}
