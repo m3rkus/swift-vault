89822eb04d87a98c274bc63900f92b3892c7f1a7
Roman <roman.anistratenkolmc@gmail.com>
2018-03-12 23:17:05 +0300

    Add notification name extension

---
+++ extensions/Notification.swift
@@ -0,0 +1,9 @@
+// MARK: - Notification helpers
+
+extension Notification.Name {
+
+    // Usage:
+    // NotificationCenter.default.post(name: .myNotification, object: nil)
+    static let myNotification = Notification.Name("myNotification")
+
+}
