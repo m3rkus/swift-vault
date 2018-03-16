1666db474c10c487318dc71fad7312442ace60d4
Roman <roman.anistratenkolmc@gmail.com>
2018-03-17 01:38:31 +0300

    Add convenient init for UIEdgeInsets

---
+++ extensions/UIEdgeInsets.swift
@@ -0,0 +1,10 @@
+// MARK: - Convenient init
+
+extension UIEdgeInsets {
+    public init(uniform: CGFloat) {
+        top = uniform
+        left = uniform
+        bottom = uniform
+        right = uniform
+    }
+}
