0ec7de9cd6db4f45a2952865d4f82162a67d55fa
Roman <roman.anistratenkolmc@gmail.com>
2018-03-25 22:35:29 +0300

    Add SettingsService concept

---
+++ concepts/SettingsService.swift
@@ -0,0 +1,49 @@
+extension UserDefaults {
+
+    subscript<T>(key: String) -> T? {
+        get {
+            return value(forKey: key) as? T
+        }
+        set {
+            set(newValue, forKey: key)
+        }
+    }
+
+    subscript<T: RawRepresentable>(key: String) -> T? {
+         get {
+             if let rawValue = value(forKey: key) as? T.RawValue {
+                 return T(rawValue: rawValue)
+             }
+             return nil
+         }
+         set {
+             set(newValue?.rawValue, forKey: key)
+         }
+    }
+}
+
+class SettingsService {
+
+    enum AppTheme: Int {
+        case light
+        case dark
+    }
+
+    var isNotificationsEnabled: Bool {
+        get {
+            return UserDefaults.standard[#function] ?? true
+        }
+        set {
+            UserDefaults.standard[#function] = newValue
+        }
+    }
+
+    var appTheme: AppTheme {
+        get {
+            return UserDefaults.standard[#function] ?? .light
+        }
+        set {
+            UserDefaults.standard[#function] = newValue
+        }
+    }
+}
