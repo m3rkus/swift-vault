4396f401f5dc1e1cdb52847a46361bce2c614965
Roman <roman.anistratenkolmc@gmail.com>
2018-04-08 23:43:01 +0300

    Add extension for optional string validation

---
+++ extensions/Optional.swift
@@ -0,0 +1,13 @@
+
+// MARK: - For optional string validation
+
+extension Optional where Wrapped == String {
+    var isNilOrEmpty: Bool {
+        switch self {
+        case let string?:
+            return string.isEmpty
+        case nil:
+            return true
+        }
+    }
+}
