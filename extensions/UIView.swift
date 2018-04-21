90a4c9fe904f8f53ecc571acd38137bfa503b931
Roman <roman.anistratenkolmc@gmail.com>
2018-04-21 13:55:06 +0300

    Add UIView extension for content changing animation with fade transition

--- extensions/UIView.swift
+++ extensions/UIView.swift
@@ -100,7 +100,7 @@ extension UIView {
 extension UIView {
 
     // Disturbs the view. Useful for getting the user's attention when something changed.
-    public func disturb() {
+    func disturb() {
         transform = CGAffineTransform(scaleX: 0.98, y: 0.98)
 
         UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 150, options: [.curveEaseOut, .beginFromCurrentState, .allowUserInteraction], animations: {
@@ -109,12 +109,22 @@ extension UIView {
     }
 
     // Shakes the view. Useful for displaying failures to users.
-    public func shake() {
+    func shake() {
         transform = CGAffineTransform(translationX: 10, y: 0)
 
         UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 50, options: [.curveEaseOut, .beginFromCurrentState, .allowUserInteraction], animations: {
             self.transform = .identity
         }, completion: nil)
+    }
+
+    // Add content changing animation with fade transition
+    func fadeTransition(_ duration: CFTimeInterval) {
+        let animation = CATransition()
+        animation.timingFunction = CAMediaTimingFunction(name:
+        kCAMediaTimingFunctionEaseInEaseOut)
+        animation.type = kCATransitionFade
+        animation.duration = duration
+        layer.add(animation, forKey: kCATransitionFade)
     }
 
 }
