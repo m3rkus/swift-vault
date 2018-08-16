495121b97657f8a0ea380c631bce06f588a10691
m3rkus <roma4271@gmail.com>
2018-08-16 14:49:01 +0300

    Add measure concept

---
+++ concepts/Measure.swift
@@ -0,0 +1,11 @@
+
+/// Measure block execution time
+
+@discardableResult
+func measure<A>(name: String = "", _ block: () -> A) -> A {
+    let startTime = CACurrentMediaTime()
+    let result = block()
+    let timeElapsed = CACurrentMediaTime() - startTime
+    print("Time: \(name) - \(timeElapsed)")
+    return result
+}
