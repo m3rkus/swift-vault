f75dd667fc1523f411b7d3cac0872f008a2d9881
m3rkus <roman4271@gmail.com>
2018-11-10 21:18:10 +0300

    Add current thread name extension

---
+++ extensions/Thread.swift
@@ -0,0 +1,14 @@
+public extension Thread {
+
+    /// Returns the name of the thread or 'main-thread', if it's the application's main thread
+    public class var currentName: String {
+
+        guard !isMainThread else { return "main-thread" }
+
+        if let threadName = current.name, !threadName.isEmpty {
+            return threadName
+        } else {
+            return String(format: "%p", current)
+        }
+    }
+}
