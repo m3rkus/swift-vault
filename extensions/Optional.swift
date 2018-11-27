86bcc4d5d63410676f6efec7fd4e2f11862ed784
m3rkus <roman4271@gmail.com>
2018-11-27 09:49:00 +0300

    Add extensions for optional

--- extensions/Optional.swift
+++ extensions/Optional.swift
@@ -3,13 +3,26 @@
 // Usage: 
 // guard !usernameTextField.text.isNilOrEmpty else { return false }
 
-extension Optional where Wrapped == String {
+extension Optional where Wrapped: Collection {
     var isNilOrEmpty: Bool {
-        switch self {
-        case let string?:
-            return string.isEmpty
-        case nil:
-            return true
+        return self?.isEmpty ?? true
+    }
+}
+
+
+// MARK: - For optional values closure based matching
+// Usage: 
+// searchBar.text.matching { $0.count > 2 }.map(performSearch)
+extension Optional {
+    func matching(_ predicate: (Wrapped) -> Bool) -> Wrapped? {
+        guard let value = self else {
+            return nil
         }
+
+        guard predicate(value) else {
+            return nil
+        }
+
+        return value
     }
 }
